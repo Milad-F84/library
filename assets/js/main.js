04c7294bbf1cc8ffeb4e57d2077ef5760480341d
miladfarajollahi <mfarajollahifrontend@gmail.com>
2024-10-13 19:50:38 +0330

    remade code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,10 @@ const root = document.getElementById("root");
 const filter = document.getElementById("filter");
 
 //functions
-function showBooks(list){
-    const template =list.map((book) => {
-        return`
+function showBooks(list) {
+  const template = list
+    .map((book) => {
+      return `
         <div class = "card">
         <img class = "card__img" src = "./assets/imgs/${book.imgSrc}"/>
         <div class = "card__desc">
@@ -13,29 +14,31 @@ function showBooks(list){
         <span>${book.author}</span>
         <span>${book.published_date}</span>
         </div>
-        </div>`
-    }).join("");
-    root.innerHTML = template;
+        </div>`;
+    })
+    .join("");
+  root.innerHTML = template;
 }
 showBooks(BOOKS);
 
-function filters(list){
-    const uniqueAuthors = [];
-    for (const item of list) {
-        if(!uniqueAuthors.includes(item.author)){
-            uniqueAuthors.push(item.author);
-        }
+function filters(list) {
+  const uniqueAuthors = [];
+  for (const item of list) {
+    if (!uniqueAuthors.includes(item.author)) {
+      uniqueAuthors.push(item.author);
     }
-    const authorTemplate = uniqueAuthors.map((item) =>
-    {
-        return`
+  }
+  const authorTemplate = uniqueAuthors
+    .map((item) => {
+      return `
         <div>
         <lable for ="${item}">${item}</lable>
         <input value = "${item}" id = "${item}" type = "checkbox"></input>
         </div>
-        `
-    }).join("");
+        `;
+    })
+    .join("");
 
-    filter.innerHTML += authorTemplate;
+  filter.innerHTML += authorTemplate;
 }
 filters(BOOKS);
